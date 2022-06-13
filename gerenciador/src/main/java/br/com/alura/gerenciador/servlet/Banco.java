ae9e7fa56c47b9a19ad2206d6e928fcfccaf3deb
Renato Cézar <renatocezarsilva@hotmail.com>
2022-06-13 16:34:29 -0300

    Criação dos atributos e métodos da classe Banco incluindo a lista estática que serviu como banco de dados improvisado para persistência.

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,5 +1,33 @@
 package br.com.alura.gerenciador.servlet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Banco {
+
+	private static List<Empresa> lista = new ArrayList<Empresa>();
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Google");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Alura");
+		Empresa empresa3 = new Empresa();
+		empresa3.setNome("Caelum");
+		Empresa empresa4 = new Empresa();
+		empresa4.setNome("Fiap");
+		lista.add(empresa);
+		lista.add(empresa2);
+		lista.add(empresa3);
+		lista.add(empresa4);		
+	}
+
+	public void adiciona(Empresa empresa) {
+		Banco.lista.add(empresa);
+	}
+
+	public List<Empresa> getEmpresas() {
+		return Banco.lista;
+	}
 
 }
